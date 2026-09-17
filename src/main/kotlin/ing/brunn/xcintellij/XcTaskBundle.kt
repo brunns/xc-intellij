33b91db1030e664d85da55ec2d7fd6885df650e5
Simon Brunning <simon@brunningonline.net>
2026-09-17 11:36:27 +0100

    Rename Bundle.

--- src/main/kotlin/ing/brunn/xcintellij/XcTaskBundle.kt
+++ src/main/kotlin/ing/brunn/xcintellij/XcTaskBundle.kt
@@ -5,9 +5,9 @@ import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.XcTaskBundle"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object XcTaskBundle : DynamicBundle(BUNDLE) {
     operator fun get(
         @PropertyKey(resourceBundle = BUNDLE) key: String,
         vararg params: Any,
